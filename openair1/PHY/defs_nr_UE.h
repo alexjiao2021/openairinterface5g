1fda7d62eb59d689460d18542b379b1c27926262
Alex Jiao <alex.jiao@keysight.com>
2026-08-25 12:03:44 +0800

    fix(phy, nr_ue): fix data races on neighbor-cell measurement state
    ThreadSanitizer found below unguarded cross-thread accesses in the NR UE PHY:
    neighboring_cell_info[] and nrUE_config.meas_config.nr_neighboring_cell[]
    are read and written by search_new_neighboring_cell() and
    do_neighboring_cell_measurements(), which can run concurrently on separate
    thread-pool workers with no synchronization between them.
    
    Add mutex to both functions.
    
    Signed-off-by: Alex Jiao <alex.jiao@keysight.com>

--- openair1/PHY/defs_nr_UE.h
+++ openair1/PHY/defs_nr_UE.h
@@ -154,6 +154,7 @@ typedef struct {
 
   /// Info about neighboring cells to perform the measurements
   neighboring_cell_info_t neighboring_cell_info[NUMBER_OF_NEIGHBORING_CELLS_MAX];
+  pthread_mutex_t neighboring_cell_mutex;
   _Atomic(bool) meas_request_pending;
   _Atomic(bool) search_new_cells_pending;
   int last_blind_slot;
